d20cd74481530ab4e5779ab0358f5698b85b8a11
PAPELITO654 <enrique.jeghgh4@gmail.com>
2024-12-11 16:08:30 -0600

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -2,7 +2,6 @@ import mysql.connector
 
 class UserModel:
     def __init__(self):
-        # Configuración de conexión a la base de datos
         self.con = mysql.connector.connect(
             host="185.232.14.52",
             database="u760464709_tst_sep",
@@ -10,19 +9,33 @@ class UserModel:
             password="dJ0CIAFF="
         )
     
-    def create_user(self, username, password):
-        """Crea un nuevo usuario en la base de datos."""
+    def verificar_conexion(self):
         if not self.con.is_connected():
             self.con.reconnect()
-        cursor = self.con.cursor()
-        sql = "INSERT INTO tst0_usuarios (Nombre_Usuario, Contrasena) VALUES (%s, %s)"
-        cursor.execute(sql, (username, password))
-        self.con.commit()
-    
-    def get_all_users(self):
-        """Obtiene todos los usuarios de la base de datos."""
-        if not self.con.is_connected():
-            self.con.reconnect()
+
+    def obtener_usuarios(self):
+        self.verificar_conexion()
         cursor = self.con.cursor()
         cursor.execute("SELECT * FROM tst0_usuarios ORDER BY Id_Usuario DESC")
         return cursor.fetchall()
+
+    def guardar_usuario(self, usuario, contrasena):
+        self.verificar_conexion()
+        cursor = self.con.cursor()
+        sql = "INSERT INTO tst0_usuarios (Nombre_Usuario, Contrasena) VALUES (%s, %s)"
+        cursor.execute(sql, (usuario, contrasena))
+        self.con.commit()
+
+    def actualizar_usuario(self, id, usuario, contrasena):
+        self.verificar_conexion()
+        cursor = self.con.cursor()
+        sql = "UPDATE tst0_usuarios SET Nombre_Usuario = %s, Contrasena = %s WHERE Id_Usuario = %s"
+        cursor.execute(sql, (usuario, contrasena, id))
+        self.con.commit()
+
+    def eliminar_usuario(self, id):
+        self.verificar_conexion()
+        cursor = self.con.cursor()
+        sql = "DELETE FROM tst0_usuarios WHERE Id_Usuario = %s"
+        cursor.execute(sql, (id,))
+        self.con.commit()
