50c0410196125ad601e295e6e85bfe0abe12b672
PAPELITO654 <enrique.jeghgh4@gmail.com>
2024-12-11 15:58:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from controllers.user_controller import user_controller
+from controllers.user_controller import user_blueprint
 
 app = Flask(__name__)
-app.register_blueprint(user_controller)
+
+# Registrar Blueprint del controlador de usuarios
+app.register_blueprint(user_blueprint)
 
 if __name__ == "__main__":
     app.run(debug=True)
